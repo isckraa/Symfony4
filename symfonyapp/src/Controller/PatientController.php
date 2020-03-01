61e6e6a550901d93d6e563c534b0c226caa09648
nelu <wf.nelu@yandex.ru>
2020-03-01 19:10:55 +0100

    Hotfix add user

--- symfonyapp/src/Controller/PatientController.php
+++ symfonyapp/src/Controller/PatientController.php
@@ -19,6 +19,7 @@ class PatientController extends AbstractController
         // INSERTION D'UN NOUVEAU PATIENT
         $patient = new Patient();
         $consultation = new Consultation();
+        $repository = $this->getDoctrine()->getManager()->getRepository(Patient::class);
 
         // HYDRATATION DU PATIENT
         $patient->setNumSS( '168077151115257' );
@@ -26,26 +27,37 @@ class PatientController extends AbstractController
         $patient->setPrenom( 'Willer' );
         $patient->setDateNaissance( new DateTime( '1978-12-09' ) );
         $patient->setSexe( "M" );
+
+        // RECHERCHE DE L'UTILISATEUR DANS BDD
+        $searchPatient = $repository->findBy(
+            array(
+                'nom'       => $patient->getNom(),
+                'prenom'    => $patient->getPrenom(),
+                'numSS'    => $patient->getNumSS()
+            )
+        );
+
+        // SI UTILISATEUR N'EXISTE PAS ALORS ON L'AJOUTE
+        if( empty( $searchPatient ) ) {
+            // AJOUTER UNE CONSULTATION
+            $consultation->setPatient( $patient );
+            $consultation->setDateHeure( new DateTime( 'NOW' ) );
+            $patient->addConsultation( $consultation );
+    
+            // RECUPERATION DU SERVICE DOCTRINE
+            $doctrine = $this->getDoctrine();
+    
+            // RECUPERATION DU SERVICE DE GESTIONNE D'ENTITES
+            $entityManager = $doctrine->getManager();
+    
+            // GARDER L'ENTITE PATIENT EN MEMOIRE
+            $entityManager->persist( $consultation );
+            $entityManager->persist( $patient );
+    
+            // OUVRE UNE TRANSACTION ET ENREGISTE TOUTES LES ENTITES
+            $entityManager->flush();
+        }
         
-        // AJOUTER UNE CONSULTATION
-        $consultation->setPatient( $patient );
-        $consultation->setDateHeure( new DateTime( 'NOW' ) );
-        $patient->addConsultation( $consultation );
-
-        // RECUPERATION DU SERVICE DOCTRINE
-        $doctrine = $this->getDoctrine();
-
-        // RECUPERATION DU SERVICE DE GESTIONNE D'ENTITES
-        $entityManager = $doctrine->getManager();
-
-        // GARDER L'ENTITE PATIENT EN MEMOIRE
-        // $entityManager->persist( $consultation );
-        // $entityManager->persist( $patient );
-
-        // OUVRE UNE TRANSACTION ET ENREGISTE TOUTES LES ENTITES
-        // $entityManager->flush();
-
-        // RETURN
         return $this->render('patient/index.html.twig', [
             'controller_name' => 'PatientController',
         ]);
